6fde663b238cf64ae465c1c85450a8d1c1b17a41
illuzen <github.handheld844@passmail.net>
2025-05-10 12:30:29 +0800

    removed unnecessary target stuff, tried to get prover benchmark to match main

--- prover/benches/prover_benchmarks.rs
+++ prover/benches/prover_benchmarks.rs
@@ -1,12 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use wormhole_circuit::inputs::CircuitInputs;
 use wormhole_prover::WormholeProver;
+use std::time::Duration;
+
+const MEASUREMENT_TIME_S: u64 = 60;
 
 fn bench_prove(c: &mut Criterion) {
-    let mut group = c.benchmark_group("prover");
-    group.sample_size(10); // Reduce sample size since proving is expensive
-
-    group.bench_function("commit_and_prove", |b| {
+    // let mut group = c.benchmark_group("prover");
+    // group.sample_size(10); // Reduce sample size since proving is expensive
+    //
+    // group.bench_function("commit_and_prove", |b| {
+    c.bench_function("prover", |b| {
         b.iter(|| {
             let prover = WormholeProver::new();
             let inputs = CircuitInputs::test_default();
@@ -14,8 +18,17 @@ fn bench_prove(c: &mut Criterion) {
         })
     });
 
-    group.finish();
+    // group.finish();
 }
 
-criterion_group!(benches, bench_prove);
-criterion_main!(benches); 
+fn benches() {
+    let mut criterion =
+        Criterion::default().measurement_time(Duration::from_secs(MEASUREMENT_TIME_S));
+
+    bench_prove(&mut criterion);
+
+    criterion.final_summary();
+}
+
+// criterion_group!(benches, bench_prove);
+criterion_main!(benches);
