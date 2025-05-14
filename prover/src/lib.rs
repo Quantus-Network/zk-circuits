150812abcc50af3aea8b385aa366c9373f042f94
NIK <nheger@gmail.com>
2025-05-14 13:13:24 +0800

    add docs back in

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -1,1 +1,26 @@
+//! Prover logic for the Wormhole circuit.
+//!
+//! This module provides the [`WormholeProver`] type, which allows committing inputs to the circuit
+//! and generating a zero-knowledge proof using those inputs.
+//!
+//! The typical usage flow involves:
+//! 1. Initializing the prover (e.g., via [`WormholeProver::default`] or [`WormholeProver::new`]).
+//! 2. Creating user inputs with [`CircuitInputs`].
+//! 3. Committing user inputs using [`WormholeProver::commit`].
+//! 4. Generating a proof using [`WormholeProver::prove`].
+//!
+//! # Example
+//!
+//! ```
+//! use wormhole_circuit::inputs::CircuitInputs;
+//! use wormhole_prover::prover::WormholeProver;
+//!
+//! # fn main() -> anyhow::Result<()> {
+//! # let inputs = CircuitInputs::default();
+//! let prover = WormholeProver::new();
+//! let proof = prover.commit(&inputs)?.prove()?;
+//! # Ok(())
+//! # }
+//! ```
+
 pub mod prover;
--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -1,1 +1,23 @@
+/// A verifier for the wormhole circuit.
+///
+///# Example
+///
+/// Create a verifier and verify a proof:
+///
+///```
+/// use wormhole_circuit::inputs::CircuitInputs;
+/// use wormhole_prover::prover::WormholeProver;
+/// use wormhole_verifier::verifier::WormholeVerifier;
+/// #
+/// # fn main() -> anyhow::Result<()> {
+/// # let inputs = CircuitInputs::default();
+/// # let prover = WormholeProver::new();
+/// # let proof = prover.commit(&inputs)?.prove()?;
+///
+/// let verifier = WormholeVerifier::new();
+/// verifier.verify(proof)?;
+/// # Ok(())
+/// # }
+/// ```
+
 pub mod verifier;
