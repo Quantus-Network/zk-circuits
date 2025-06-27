6a04b38644432ef92631eeaacc2a6c49ab434824
Veronika Romashkina <vrom911@gmail.com>
2025-06-27 23:25:45 +0100

    Fix dummy proof tests

--- wormhole/aggregator/src/util.rs
+++ wormhole/aggregator/src/util.rs
@@ -1,17 +1,12 @@
-use anyhow::{bail, Context};
+use anyhow::bail;
 use plonky2::plonk::circuit_data::CommonCircuitData;
 use wormhole_verifier::ProofWithPublicInputs;
 use zk_circuits_common::circuit::{C, D, F};
 
-#[cfg(not(feature = "no_zk"))]
-const DUMMY_PROOF_BYTES: &[u8] = include_bytes!("../data/dummy_proof_zk.bin");
-#[cfg(feature = "no_zk")]
-const DUMMY_PROOF_BYTES: &[u8] = include_bytes!("../data/dummy_proof.bin");
-
 pub fn pad_with_dummy_proofs(
     mut proofs: Vec<ProofWithPublicInputs<F, C, D>>,
     proof_len: usize,
-    common_data: &CommonCircuitData<F, D>,
+    _common_data: &CommonCircuitData<F, D>,
 ) -> anyhow::Result<Vec<ProofWithPublicInputs<F, C, D>>> {
     let num_proofs = proofs.len();
 
@@ -19,8 +14,15 @@ pub fn pad_with_dummy_proofs(
         bail!("proofs to aggregate was more than the maximum allowed")
     }
 
-    let dummy_proof = ProofWithPublicInputs::from_bytes(DUMMY_PROOF_BYTES.to_vec(), common_data)
-        .context("failed to deserialize dummy proof")?;
+    if num_proofs == proof_len {
+        return Ok(proofs);
+    }
+
+    if num_proofs == 0 {
+        bail!("cannot pad an empty list of proofs");
+    }
+
+    let dummy_proof = proofs.last().unwrap().clone();
     for _ in 0..(proof_len - num_proofs) {
         proofs.push(dummy_proof.clone());
     }
