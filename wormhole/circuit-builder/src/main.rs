aa6450495c012caa9023d60ca2582775963fac85
illuzen <illuzen@users.noreply.github.com>
2025-06-30 17:27:06 +0800

    Update wormhole/circuit-builder/src/main.rs
    Co-authored-by: Jonathan <zeapo@pm.me>

--- wormhole/circuit-builder/src/main.rs
+++ wormhole/circuit-builder/src/main.rs
@@ -23,26 +23,22 @@ fn main() {
     let prover_data = circuit_data.prover_data();
     let common_data = &verifier_data.common;
 
-    create_dir_all("generated-bins").unwrap();
+    create_dir_all("generated-bins")?;
 
     // Serialize common data
-    let common_bytes = common_data.to_bytes(&gate_serializer).unwrap();
-    let mut file = File::create("generated-bins/common.bin").unwrap();
-    file.write_all(&common_bytes).unwrap();
+    let common_bytes = common_data.to_bytes(&gate_serializer)?;
+    fs::write("generated-bins/common.bin", common_bytes)?;
     println!("Common data saved to generated-bins/common.bin");
 
     // Serialize verifier only data
-    let verifier_only_bytes = verifier_data.verifier_only.to_bytes().unwrap();
-    let mut file = File::create("generated-bins/verifier.bin").unwrap();
-    file.write_all(&verifier_only_bytes).unwrap();
+    let verifier_only_bytes = verifier_data.verifier_only.to_bytes()?;
+    fs::write("generated-bins/verifier.bin", common_bytes)?;
     println!("Verifier data saved to generated-bins/verifier.bin");
 
     // Serialize prover only data
     let prover_only_bytes = prover_data
         .prover_only
-        .to_bytes(&generator_serializer, common_data)
-        .unwrap();
-    let mut file = File::create("generated-bins/prover.bin").unwrap();
-    file.write_all(&prover_only_bytes).unwrap();
+        .to_bytes(&generator_serializer, common_data)?;
+    fs::write("generated-bins/prover.bin", common_bytes)?;
     println!("Prover data saved to generated-bins/prover.bin");
 }
